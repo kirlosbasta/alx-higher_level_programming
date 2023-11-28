cef0976fae55ca023efb88c00a0203c5c284cf44
kirlos basta <kirlosbasta@gmail.com>
2023-11-28 02:16:13 +0200

    Add 10-linked_lists.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if a singly linked list is a cycle or not
+ * @list: head of the list
+ *
+ * Return: 0 if list has no cycle and 1 it has
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *speed1, *speed2;
+
+	speed1 = list;
+	speed2 = list;
+	while (speed2 != NULL)
+	{
+		speed1 = speed1->next;
+		if (speed2 != NULL)
+			speed2 = speed2->next;
+		if (speed2 != NULL)
+			speed2 = speed2->next;
+		if (speed1 == speed2)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
