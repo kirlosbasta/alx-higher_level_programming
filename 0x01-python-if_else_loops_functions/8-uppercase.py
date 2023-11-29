3a0f7e7be18829ea654c4192c76a43c352682192
kirlos basta <kirlosbasta@gmail.com>
2023-11-29 02:12:46 +0200

    Add 8-islower.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """convert a string to uppercase"""
+    new_str = ""
+    for i in range(len(str)):
+        if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
+            new_str += chr(ord(str[i]) - 32)
+        else:
+            new_str += str[i]
+    print(new_str)
