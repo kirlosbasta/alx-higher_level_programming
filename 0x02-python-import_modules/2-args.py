a999c9b80cb8355ec116cfa39ea21bded16b55eb
kirlos basta <kirlosbasta@gmail.com>
2023-11-30 21:25:21 +0200

    Add 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
         print("0 arguments.")
     elif length == 1:
         print("1 argument:")
-        print("1 {:s}".format(argv[1]))
+        print("1: {:s}".format(argv[1]))
     else:
         print("{:d} arguments:".format(length))
         for i in range(1, length + 1):
