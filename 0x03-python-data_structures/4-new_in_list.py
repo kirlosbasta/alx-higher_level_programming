92c59e3c02bdc2d40fe037f91b9de6998cd0da4e
kirlos basta <kirlosbasta@gmail.com>
2023-12-02 17:41:58 +0200

    Add 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0 or idx >= len(my_list):
+        new_list = my_list
+        return new_list
+    for i in range(len(my_list)):
+        if i == idx:
+            new_list.append(element)
+        else:
+            new_list.append(my_list[i])
+    return new_list
