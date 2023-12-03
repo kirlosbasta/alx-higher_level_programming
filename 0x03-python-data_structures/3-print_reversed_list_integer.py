42ac79519aaf6f1d3481f5bd5f3e0b59b4dd55b2
kirlos basta <kirlosbasta@gmail.com>
2023-12-03 03:27:24 +0200

    Add 11-delete_at.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        return my_list
     rev_list = my_list
     rev_list.reverse()
     for x in rev_list:
