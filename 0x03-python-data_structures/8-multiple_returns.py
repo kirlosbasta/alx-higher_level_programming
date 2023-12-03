2b1d8fb7682def6868880c9f21edcb5d9df35289
kirlos basta <kirlosbasta@gmail.com>
2023-12-03 02:51:26 +0200

    Add 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        return 0, None
+        return None, None
     else:
         length = len(sentence)
         return length, sentence[0]
