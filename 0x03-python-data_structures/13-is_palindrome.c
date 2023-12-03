fb540dac3c7ddb233290febab149a1fe6f74cee9
kirlos basta <kirlosbasta@gmail.com>
2023-12-04 00:32:57 +0200

    Add palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - check if a number in reverse is the same as normal
+ * @head: head of the list
+ *
+ * Return: 0 if it's not palindrome 1 if it's
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int *list = malloc(sizeof(int) * 10);
+	int size_list = 10;
+	int count = 0, i;
+	listint_t *current = *head;
+
+	if (current == NULL)
+	{
+		free(list);
+		return (1);
+	}
+	while (current != NULL)
+	{
+		if (count > 9)
+		{
+			list = realloc(list, size_list + 1);
+			size_list++;
+		}
+		list[count++] = current->n;
+		current = current->next;
+	}
+	for (i = 0; i < (count / 2); i++)
+	{
+		if (list[i] != list[count - 1 - i])
+		{
+			free(list);
+			return (0);
+		}
+	}
+	free(list);
+	return (1);
+}
