14f5c8d151cdc595f84545079afb8fe0b05eeb79
kirlos basta <kirlosbasta@gmail.com>
2023-12-18 20:23:37 +0200

    Add 1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """print and return True if value is intger and false if not"""
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
