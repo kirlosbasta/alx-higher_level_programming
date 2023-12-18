9a26106eb22c4d013117f402f2f7aa8a4366e410
kirlos basta <kirlosbasta@gmail.com>
2023-12-18 19:54:00 +0200

    Add safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x == 0 or x < 0:
-        return 0
+    """return number of items in a list and print it"""
     try:
         i = 0
         while i < x:
