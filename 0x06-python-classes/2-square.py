ae5250960b244d3af197a27ea3dd921f61d05645
kirlos basta <kirlosbasta@gmail.com>
2023-12-20 18:57:36 +0200

    Add 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is a demo module for square class with private size"""
+
+
+class Square:
+    """Square class with size"""
+
+    def __init__(self, size=0):
+        """initalize a new square
+
+            Args:
+                size(int): The size of square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
