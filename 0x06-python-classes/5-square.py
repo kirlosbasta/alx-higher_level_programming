c86999f2da9332b18d8b5ab51f75412ae18ad3df
kirlos basta <kirlosbasta@gmail.com>
2023-12-20 20:44:20 +0200

    Add 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""This is a demo module for square class with private size"""
+
+
+class Square:
+    """Square class with size"""
+
+    def __init__(self, size=0):
+        """initalize a new square
+
+            Args:
+                size(int): The size of square
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the size of square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        """Validate the size"""
+
+        if not isinstance(val, int):
+            raise TypeError('size must be an integer')
+        elif val < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = val
+
+    def area(self):
+        """Return the size of square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square"""
+
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print('#', end='')
+            print('')
+        if self.__size == 0:
+            print('')
