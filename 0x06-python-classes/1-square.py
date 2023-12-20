2dd53e3cbfabe2471316bd989db2fba61f918c70
kirlos basta <kirlosbasta@gmail.com>
2023-12-20 18:40:24 +0200

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is a demo module for square class with private size attribute"""
+
+
+class Square:
+    """Square class with size"""
+    def __init__(self, size):
+        self.__size = size
