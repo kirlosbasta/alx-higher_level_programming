cb2267465aa1eac345a94cd12100da229b4ed67c
kirlos basta <kirlosbasta@gmail.com>
2023-12-21 04:06:38 +0200

    Add 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -9,7 +9,6 @@ class MagicClass:
     def __init__(self, radius=0):
         """initialize the radius"""
 
-        self.__radius = 0
         if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
