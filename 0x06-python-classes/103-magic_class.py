aedd189df43dbe28e6be7567e3ffb32475ac7318
kirlos basta <kirlosbasta@gmail.com>
2023-12-21 04:11:16 +0200

    Add 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -9,7 +9,7 @@ class MagicClass:
     def __init__(self, radius=0):
         """initialize the radius"""
 
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
-Square = __import__('102-square').Square
+MagicClass = __import__('103-magic_class').MagicClass
 
-s_5 = Square(5)
-s_6 = Square(6)
-
-if s_5 < s_6:
-    print("Square 5 < Square 6")
-if s_5 <= s_6:
-    print("Square 5 <= Square 6")
-if s_5 == s_6:
-    print("Square 5 == Square 6")
-if s_5 != s_6:
-    print("Square 5 != Square 6")
-if s_5 > s_6:
-    print("Square 5 > Square 6")
-if s_5 >= s_6:
-    print("Square 5 >= Square 6")
+mc = MagicClass(12)
+print('{}'.format(mc.area()))
