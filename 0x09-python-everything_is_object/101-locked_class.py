507fae47b286a082e3c35ae16ae2ba5ebbc31cf3
kirlos basta <kirlosbasta@gmail.com>
2024-01-06 07:10:54 +0200

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for locked Class only"""
+
+
+from typing import Any
+
+
+class LockedClass:
+    """Class that can set only first_name as it attribute any thing
+        else raise attribute error
+    """
+    def __setattr__(self, name, value):
+        if name != "first_name":
+            raise AttributeError('object has no attribute {}'.format(name))
+        self.__dict__[name] = value
