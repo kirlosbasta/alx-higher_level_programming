5bcf332fba868d54caeace67f2411bed6d8e9d3e
kirlos basta <kirlosbasta@gmail.com>
2024-01-09 04:47:00 +0200

    Add 101-add_attribute.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -13,14 +13,10 @@ class Square(Rectangle):
         """initalize each instantiation with size"""
 
         super().integer_validator('size', size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
         """return the area of square"""
 
         return self.__size ** 2
-
-    def __str__(self):
-        """print user friendly representation of Rectangle"""
-
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
