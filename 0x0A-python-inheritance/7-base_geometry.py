01b2a5430adf45808d717ff5431a292de7b9fa3b
kirlos basta <kirlosbasta@gmail.com>
2024-01-09 02:54:37 +0200

    Add 8-rectangle.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
             name: string
             value: must be more that 0
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
