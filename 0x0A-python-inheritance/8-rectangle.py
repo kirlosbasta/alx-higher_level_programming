b2c8672b5339f089bd729078a986cfa788c6d2c1
kirlos basta <kirlosbasta@gmail.com>
2024-01-09 04:32:55 +0200

    Add 101-add_attribute.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python3
 """Module contain BaseGeometry class"""
-
-
-class BaseGeometry:
-    """Class that has basic geometry features"""
-
-    def area(self):
-        """raise an Exception message"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """
-        Validate value
-        Args:
-            name: string
-            value: must be more that 0
-        """
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
