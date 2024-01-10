41770d9ea4766183f02913872148dd4f98b3ad8d
kirlos basta <kirlosbasta@gmail.com>
2024-01-10 12:38:00 +0200

    100-append_after.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -22,6 +22,9 @@ class Student:
 
     def reload_from_json(self, json):
         '''replaces all attributes of the Student instance'''
-        self.first_name = json['first_name']
-        self.last_name = json['last_name']
-        self.age = json['age']
+        if 'first_name' in json:
+            self.first_name = json['first_name']
+        if 'last_name' in json:
+            self.last_name = json['last_name']
+        if 'age' in json:
+            self.age = json['age']
