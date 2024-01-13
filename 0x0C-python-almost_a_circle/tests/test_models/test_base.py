8318cd7ad383199e48be6d851eb1d1a530bb972d
kirlos basta <kirlosbasta@gmail.com>
2024-01-14 00:39:39 +0200

    Add square.py, rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,6 +3,7 @@
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -64,8 +65,82 @@ class TestBase(unittest.TestCase):
         with open("Rectangle.json", "r") as file:
             acutal = file.read()
             res = '[{"y": 8, "x": 2, "id": 1, "width": 10, "height": 7},\
-                {"y": 0, "x": 0, "id": 2, "width": 2, "height": 4}]'
-            self.assertEqual(acutal, res)
+ {"y": 0, "x": 0, "id": 2, "width": 2, "height": 4}]'
+            self.assertEqual(len(acutal), len(res))
+
+    def test_save_emp(self):
+        '''test save to file'''
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_r = []
+        Rectangle.save_to_file(list_r)
+        with open("Rectangle.json", "r") as file:
+            acutal = file.read()
+            res = '[]'
+            self.assertEqual(len(acutal), len(res))
+
+    def test_save_None(self):
+        '''test save to file'''
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_r = []
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as file:
+            acutal = file.read()
+            res = '[]'
+            self.assertEqual(len(acutal), len(res))
+
+    def test_save_no_arg(self):
+        '''test save to file'''
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        with self.assertRaises(TypeError) as e:
+            Rectangle.save_to_file()
+            err = "Base.save_to_file() missing 1 required positional argument: 'list_objs'"
+            self.assertEqual(str(e.exception), err)
+
+    def test_S_save_to_file(self):
+        '''test save to file'''
+        r1 = Square(10, 2, 8)
+        r2 = Square(2, 4)
+        list_r = [r1, r2]
+        Square.save_to_file(list_r)
+        with open("Square.json", "r") as file:
+            acutal = file.read()
+            res = '[{"y": 8, "x": 2, "id": 1, "size": 10},\
+ {"y": 0, "x": 0, "id": 2, "size": 2}]'
+            self.assertEqual(len(acutal), len(res))
+
+    def test_save_emp_S(self):
+        '''test save to file'''
+        r1 = Square(10, 2, 8)
+        r2 = Square(2, 4)
+        list_r = []
+        Square.save_to_file(list_r)
+        with open("Square.json", "r") as file:
+            acutal = file.read()
+            res = '[]'
+            self.assertEqual(len(acutal), len(res))
+
+    def test_save_None_S(self):
+        '''test save to file'''
+        r1 = Square(10, 7, 2, 8)
+        r2 = Square(2, 4)
+        list_r = []
+        Square.save_to_file(None)
+        with open("Square.json", "r") as file:
+            acutal = file.read()
+            res = '[]'
+            self.assertEqual(len(acutal), len(res))
+
+    def test_save_no_arg_S(self):
+        '''test save to file'''
+        r1 = Square(10, 7, 2, 8)
+        r2 = Square(2, 4)
+        with self.assertRaises(TypeError) as e:
+            Square.save_to_file()
+            err = "Base.save_to_file() missing 1 required positional argument: 'list_objs'"
+            self.assertEqual(str(e.exception), err)
 
 
 if __name__ == '__main__':
