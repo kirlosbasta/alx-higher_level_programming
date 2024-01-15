701a0eae54d88be3c792e2c087d31d01451c66d9
kirlos basta <kirlosbasta@gmail.com>
2024-01-15 19:55:10 +0200

    Add draw

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-'''Module contain Rectangle that inherits from Base
-Module contain Rectangle that inherits from Base
-Module contain Rectangle that inherits from Base
+'''
 Module contain Rectangle that inherits from Base
 '''
 from models.base import Base
