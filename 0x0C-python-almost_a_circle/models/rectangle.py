5e075f07ad42f074d61aadb835aeba7ae2d8cc83
kirlos basta <kirlosbasta@gmail.com>
2024-01-15 20:20:16 +0200

    Add draw

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,17 +110,16 @@ class Rectangle(Base):
         '''
         if args:
             for i in range(len(args)):
-                match i:
-                    case 0:
-                        self.id = args[i]
-                    case 1:
-                        self.width = args[i]
-                    case 2:
-                        self.height = args[i]
-                    case 3:
-                        self.x = args[i]
-                    case 4:
-                        self.y = args[i]
+                if i == 0:
+                    self.id = args[i]
+                elif i == 1:
+                    self.width = args[i]
+                elif i == 2:
+                    self.height = args[i]
+                elif i == 3:
+                    self.x = args[i]
+                elif i == 4:
+                    self.y = args[i]
         else:
             for key, value in kwargs.items():
                 if key == 'width':
