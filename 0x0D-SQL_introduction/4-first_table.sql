2bfae4f46b5994d109f8b8c212aa7bce551af265
kirlos basta <kirlosbasta@gmail.com>
2024-02-14 03:59:28 +0200

    Add task 6

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- creates a table called first_table
 -- doesn't fail if already exist
-REATE TABLE IF NOT EXISTS first_table (id INT,
+CREATE TABLE IF NOT EXISTS first_table (id INT,
 name VARCHAR(256));
