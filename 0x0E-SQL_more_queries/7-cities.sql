bb8831ddadfcd8ac6dbf55aa6390a38f9a8207ae
kirlos basta <kirlosbasta@gmail.com>
2024-02-15 04:54:12 +0200

    Add task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    states_id INT,
+    states_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY (states_id) REFERENCES states(id));
