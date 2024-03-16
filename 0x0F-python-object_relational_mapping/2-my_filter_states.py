c7e6eadad4526aac190ac11b4d238d3dc7bc50f6
kirlos basta <kirlosbasta@gmail.com>
2024-03-16 21:28:16 +0200

    Add 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument'''
+import sys
+import MySQLdb
+
+
+def main():
+    '''Dirver function'''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    search_name = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', user=username, passwd=password,
+                         port=3306, database=database_name)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(search_name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == '__main__':
+    main()
