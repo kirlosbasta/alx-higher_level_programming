c33ff7fa719192a5de66768976ffb16c5a96df45
kirlos basta <kirlosbasta@gmail.com>
2024-03-16 21:43:06 +0200

    Add 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ def main():
                          port=3306, database=database_name)
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name = '{}'\
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY '{}'\
                 ORDER BY states.id ASC""".format(search_name))
     rows = cur.fetchall()
     for row in rows:
