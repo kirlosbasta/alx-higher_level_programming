14b0e8c00ae2ea50edc8215ac4bb0b97619e45a1
kirlos basta <kirlosbasta@gmail.com>
2024-03-28 02:28:13 +0200

    Add body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-http_status=$(curl -so /dev/null $1 -w %{http_code})
-if [ "$http_status" -eq 200 ]; then
-    curl "$1"
-fi
+curl -sL "$1"
