33f4b73453c28e138cd371aede6acf2f887e06a3
kirlos basta <kirlosbasta@gmail.com>
2024-03-28 02:04:20 +0200

    Add body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -w "%{size_download}" "$1"
+#curl -s -w "%{size_download}" "$1"
+curl -so /dev/null "$1" -w '%{size_download}'; echo ""
