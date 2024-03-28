51818f55c422b7e6ed55ed6ee14747ac4de3862a
kirlos basta <kirlosbasta@gmail.com>
2024-03-28 05:02:57 +0200

    Add 5-post_params.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl --json @"$2" -s "$1"
+curl -H "Content-Type: application/json" -d @"$2" -s "$1"
