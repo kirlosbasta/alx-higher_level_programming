6ebe32631f27cfd0447654376531909db994d7a8
kirlos basta <kirlosbasta@gmail.com>
2024-03-28 04:17:44 +0200

    Add methods

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -i -L -sX OPTIONS 0.0.0.0:5000/route_4 | grep Allow | cut -d ' ' -f 2-
+curl -i -sX OPTIONS 0.0.0.0:5000/route_4 | grep Allow | cut -d ' ' -f 2-
